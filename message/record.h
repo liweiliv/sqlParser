72c6eca32b5d30fec8508019eee20a6314f77c2f
liweiliv <liwei960067495@sina.com>
2019-04-04 18:58:19 +0800

    Update record.h
    new

--- message/record.h
+++ message/record.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "../metaDataCollection.h"
 namespace DATABASE_INCREASE
 {
 struct recordHead
@@ -42,34 +43,28 @@ enum RecordType
     HEARTBEAT
 };
 #define COLUMN_FLAG_VIRTUAL 0x01
+#define COLUMN_FLAG_SIGNED 0x02
+
 struct columnDef
 {
     uint8_t type;
+	uint8_t srcType;
     uint8_t flag;
     uint16_t charsetID;
     uint32_t size;
+	uint32_t precision;
+	uint32_t decimals;
+	uint32_t nameOffset;
+	uint32_t setOrEnumInfoOffset;
 };
 struct record
 {
     const char * data;
     recordHead *head;
-    void * userData;
-    void * table;
-    void * transaction;
-    union{
-        uint64_t * hash;
-        void ** queue;
-    };
-    uint16_t hashCount;
     record(){}
     record(const char * data){
         this->data = data;
         head = (recordHead*)data;
-        userData = NULL;
-        table = NULL;
-        hash = NULL;
-        transaction = NULL;
-        hashCount = 0;
     }
 };
 #define TEST_BITMAP(m,i) (((m)[(i)>>3]>>(i&0x7))&0x1)
@@ -77,64 +72,80 @@ struct record
 struct TableMetaMessage :public record
 {
     /*--------------version 0----------------*/
-    /*[64 bit tableMetaID][32 bit tableVersion][8 bit database size][database string+ 1 byte '\0'][8 bit table size][table string+ 1 byte '\0']
-     * [8 bit charset size][charset string+ 1 byte '\0'][16 bit columnCount][columnDef 1]...[columnDef n]
-     * [16 bit primaryKeyColumnCount][16 bit column id 1]...[16 bit column id n]
-     * [16 bit uniqueKeyCount][16 bit uk 1 column count]...[16 bit uk n column count]
-     *[16 bit uk 1 column 1 id ]...[16 bit uk 1 column n id ]
-      * ...
-      * [16 bit uk m column 1 id ]...[16 bit uk m column n id ]
+    /*[64 bit tableMetaID][16 bit charsetID][16 bit column count][16 bit pk columns count][16 bit uk count]
+	 *[8 bit database size][database string+ 1 byte '\0'][8 bit table size][table string+ 1 byte '\0']
+     * [columnDef 1]...[columnDef n]
+     * [16 bit pk column id 1]...[16 bit pk column id n]
+     * [16 bit uk 1 column count]...[16 bit uk n column count]
+     *[32 bit uk 1 name offset][16 bit uk 1 column 1 id ]...[16 bit uk 1 column n id ]
+     * ...
+     *[32 bit uk m name offset][16 bit uk m column 1 id ]...[16 bit uk m column n id ]
+	 *[32 bit column names size]
+	 *[column 1 name string+'\0']...[column n name string+'\0']
+	 *[32 bit unique key names size]
+	 *[uk 1 name string+'\0']...[uk n name string+'\0']
+	 *[32 bit enum or set value lists size]
+	 *[enum or set value list 0+'\0']...[enum or set value list m+'\0']
      * */
-    uint64_t tableMetaID;
+	struct tableMetaHead {
+		uint64_t tableMetaID;
+		uint16_t charsetId;
+		uint16_t columnCount;
+		uint16_t primaryKeyColumnCount;
+		uint16_t uniqueKeyCount;
+	};
+	tableMetaHead metaHead;
     const char * database;
     const char * table;
-    const char * charset;
-    uint16_t columnCount;
     const columnDef *columns;
-    uint16_t primaryKeyColumnCount;
     const uint16_t * primaryKeys;
-    uint16_t uniqueKeyCount;
     const uint16_t * uniqueKeyColumnCounts;
+	uint32_t* uniqueKeyNameOffset;
     uint16_t ** uniqueKeys;
     /*-----------------------------------------------*/
     TableMetaMessage(const char * data):record(data){
-        const char * ptr = data;
-        tableMetaID = *(uint64_t*)(ptr+head->headSize);
-        database = ptr+head->headSize+sizeof(tableMetaID)+1;
-        table = database+*(uint8_t*)(database-1);
-        charset = table+*(uint8_t*)(table-1);
-        columnCount = *(uint16_t*)(charset+*(uint8_t*)(charset-1));
-        columns = (columnDef*)(charset+*(uint8_t*)(charset-1)+sizeof(columnCount));
-        ptr = (const char*)&columns[columnCount];
-        assert((uint32_t)(ptr-data)<head->headSize+sizeof(primaryKeyColumnCount));
-        primaryKeyColumnCount = *(uint16_t*)ptr;
-        if(primaryKeyColumnCount !=0)
+		const char * ptr = data + +head->headSize;
+		memcpy(&metaHead, ptr, sizeof(metaHead));
+		ptr += sizeof(metaHead);
+        database = ptr+sizeof(uint8_t);
+        table = database+*(uint8_t*)(database- sizeof(uint8_t));
+		ptr = table + *(uint8_t*)(table - sizeof(uint8_t));
+
+        columns = (columnDef*)ptr;
+        ptr = (const char*)&columns[metaHead.columnCount];
+        if(metaHead.primaryKeyColumnCount !=0)
         {
-            primaryKeys = (uint16_t*)(ptr+sizeof(primaryKeyColumnCount));
-            ptr = (const char*)&primaryKeys[primaryKeyColumnCount];
+			primaryKeys = (uint16_t*)ptr;
+            ptr = (const char*)&primaryKeys[metaHead.primaryKeyColumnCount];
         }
         else
         {
             primaryKeys = NULL;
-            ptr += sizeof(primaryKeyColumnCount);
         }
-        uniqueKeyCount = *(uint16_t*)ptr;
-        if(uniqueKeyCount>0)
+        if(metaHead.uniqueKeyCount>0)
         {
-            uniqueKeyColumnCounts = (uint16_t*)(ptr+sizeof(uniqueKeyCount));
-            ptr = (const char*)&uniqueKeyColumnCounts[uniqueKeyCount];
-            uniqueKeys = (uint16_t**)malloc(sizeof(uint16_t*)*uniqueKeyCount);
-            for(uint16_t idx =0;idx<uniqueKeyCount;idx++)
+            uniqueKeyColumnCounts = (uint16_t*)(ptr);
+            ptr = (const char*)&uniqueKeyColumnCounts[metaHead.uniqueKeyCount];
+            uniqueKeys = (uint16_t**)malloc(sizeof(uint16_t*)*metaHead.uniqueKeyCount);
+			uniqueKeyNameOffset = (uint32_t*)malloc(sizeof(uint32_t)*metaHead.uniqueKeyCount);
+            for(uint16_t idx =0;idx< metaHead.uniqueKeyCount;idx++)
             {
-                uniqueKeys[idx] = (uint16_t*)ptr;
+				uniqueKeyNameOffset[idx] = *(uint32_t*)ptr;
+                uniqueKeys[idx] = (uint16_t*)(ptr+sizeof(uint32_t));
                 ptr = (const char*)&uniqueKeys[idx][uniqueKeyColumnCounts[idx]];
             }
         }
         else
         {
-            uniqueKeyColumnCounts = NULL;
-            uniqueKeys = NULL;
+			uniqueKeyNameOffset = nullptr;
+            uniqueKeyColumnCounts = nullptr;
+            uniqueKeys = nullptr;
         }
+		ptr += *(uint32_t*)ptr + sizeof(uint32_t);//jump over column names
+		ptr += *(uint32_t*)ptr + sizeof(uint32_t);//jump over uk names
+		ptr += *(uint32_t*)ptr + sizeof(uint32_t);//jump over enum and set value lists
+		if (head->version == 0)
+			assert(ptr = data + head->size);
         /*if version increased in future,add those code:
           if(head->version >0)
           {do some thing}
@@ -143,6 +154,23 @@ struct TableMetaMessage :public record
              ...
            */
     }
+	inline const char * columnName(uint16_t columnIndex)
+	{
+		return data+columns[columnIndex].nameOffset;
+	}
+	inline bool setOrEnumValues(uint16_t columnIndex,const char *& base,uint16_t *&valueList,uint16_t &valueListSize)
+	{
+		if (columns[columnIndex].setOrEnumInfoOffset != 0)
+		{
+			base = data + columns[columnIndex].setOrEnumInfoOffset;
+			valueListSize = *(uint16_t*)base;
+			valueList = (uint16_t*)(base + sizeof(valueListSize));
+
+			return true;
+		}
+		else
+			return false;
+	}
     ~TableMetaMessage()
     {
         if(uniqueKeys!=NULL)
@@ -152,44 +180,41 @@ struct TableMetaMessage :public record
 struct DMLRecord:public record
 {
     /*--------------version 0----------------*/
-    /*[64 bit tableMetaID][32 bit tableVersion][16 bit columnCount]
+    /*[64 bit tableMetaID]
      * [32 byte new column 1 size]...[32 byte new column n size] (if exist new column)
      * [new column 1+ 1byte '\0']...[new column n+ 1byte '\0']
      * [old column bit bitmap] (if update)
      * [32 byte old column 1 size]...[32 byte old column n size] (if exist old column)
      * [old column 1+ 1byte '\0']...[old column n+ 1byte '\0']
      * */
-    struct DMLRecordHead{
-    uint64_t tableMetaID;
-    uint32_t tableVersion;
-    uint16_t columnCount;
-    };
+	uint64_t tableMetaID;
     bool outerMem;
-    DMLRecordHead *dmlHead;
-    TableMetaMessage * meta;
+	tableMeta * meta;
     const char ** newColumns;
     uint32_t * oldColumnSizes;
     const char ** oldColumns;
     uint8_t *updatedBitmap;
     DMLRecord(){}
     /*----------------------------------------------*/
-    DMLRecord(const char * data,void * mem = nullptr)  :
+    DMLRecord(const char * data,metaDataCollection * mc, void * mem = nullptr)  :
         record(data),outerMem(mem!=nullptr)
     {
         const char * ptr = data+head->headSize;
-        dmlHead = (DMLRecordHead*)ptr;
-        meta = NULL;
-        ptr += sizeof(DMLRecordHead);
+		tableMetaID = *(uint64_t*)ptr;
+		meta = mc->get(tableMetaID);
+		if (meta == nullptr)
+			return;
+        ptr += sizeof(tableMetaID);
         newColumns = oldColumns = NULL;
         /*alloc mem one time*/
         if(mem == nullptr)
-            mem = malloc(dmlHead->columnCount*sizeof(const char*)*(head->type==UPDATE||head->type==REPLACE)?2:1);
+            mem = malloc(meta->m_columnsCount *sizeof(const char*)*(head->type==UPDATE||head->type==REPLACE)?2:1);
         if(head->type == INSERT||head->type == UPDATE)
         {
-            ptr+=sizeof(uint32_t)*dmlHead->columnCount;
+            ptr+=sizeof(uint32_t)*meta->m_columnsCount;
             newColumns = (const char **)mem;
-            mem = (char*)mem+dmlHead->columnCount;
-            for(uint16_t idx = 0;idx<dmlHead->columnCount;idx++)
+            mem = (char*)mem+ meta->m_columnsCount;
+            for(uint16_t idx = 0;idx< meta->m_columnsCount;idx++)
             {
                 newColumns[idx] = ptr;
             }
@@ -197,14 +222,14 @@ struct DMLRecord:public record
         if(head->type == UPDATE)
         {
             updatedBitmap = (uint8_t*)ptr;
-            ptr+= (dmlHead->columnCount>>3)+(dmlHead->columnCount&0x7f?1:0);
+            ptr+= (meta->m_columnsCount >>3)+(meta->m_columnsCount &0x7f?1:0);
         }
         if(head->type == DELETE||head->type == UPDATE)
         {
             oldColumnSizes = (uint32_t*)ptr;
-            ptr+=sizeof(uint32_t)*dmlHead->columnCount;
+            ptr+=sizeof(uint32_t)*meta->m_columnsCount;
             oldColumns = (const char**)mem;
-            for(uint16_t idx = 0;idx<dmlHead->columnCount;idx++)
+            for(uint16_t idx = 0;idx< meta->m_columnsCount;idx++)
             {
                 oldColumns[idx] = ptr;
                 ptr+= oldColumnSizes[idx];
