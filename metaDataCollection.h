b505e0fd3acac533c3861eff9465499cc8da8a53
liweiliv <liwei960067495@sina.com>
2019-04-04 18:57:14 +0800

    Update metaDataCollection.h
    new

--- metaDataCollection.h
+++ metaDataCollection.h
@@ -10,10 +10,14 @@
 
 #include <string>
 #include "trieTree.h"
-#include <unorderMapUtil.h>
-#include "skiplist.h"
+#include "util/unorderMapUtil.h"
+#include "util/skiplist.h"
 struct tableMeta;
 struct columnMeta;
+struct charsetInfo;
+namespace STORE{
+class client;
+}
 namespace sqlParser
 {
 class sqlParser;
@@ -23,7 +27,7 @@ struct handle;
 struct Table;
 struct databaseInfo;
 }
-typedef std::tr1::unordered_map<const char *,uint32_t,StrHash,StrCompare> CharsetTree ;
+typedef std::unordered_map<const char *,const  charsetInfo*,StrHash,StrCompare> CharsetTree ;
 struct tableMetaWrap
 {
     uint64_t tableID;
@@ -52,15 +56,20 @@ class metaDataCollection
 private:
     trieTree m_dbs;
     CharsetTree m_charsetSizeList;
-    std::string m_defaultCharset;
+    const charsetInfo * m_defaultCharset;
+	leveldb::Arena m_arena;
+	tableIDComparator m_cmp;
     leveldb::SkipList<tableMetaWrap*, tableIDComparator> m_allTables;
     sqlParser::sqlParser * m_SqlParser;
+	STORE::client * m_client;
     uint64_t m_maxTableId;
+
 public:
-    metaDataCollection();
+    metaDataCollection(STORE::client *client = nullptr);
     ~metaDataCollection();
     tableMeta * get(uint64_t tableID);
     tableMeta * get(const char * database,const char * table,uint64_t originCheckPoint);
+	tableMeta * getFromRemote(uint64_t tableID);
     int put(const char * database,const char * table,tableMeta * meta,uint64_t originCheckPoint);
     int purge(uint64_t originCheckPoint);
     int processDDL(const char * ddl,uint64_t originCheckPoint);
